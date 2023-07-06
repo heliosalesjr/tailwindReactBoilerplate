06f23cbc17bf13773a1d63cad171d99e889b7bab
heliosalesjr <heliosalesjr@gmail.com>
2023-07-06 12:19:21 -0300

    1st commit: a React / Vite / Tailwind boilerplate to save time on starting new projects

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <div className='bg-purple-500'>
+        <h1 className='text-3xl font-bold underline'>Olare Ooô</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo unde maiores quod harum ea id sed odio dolore sint nostrum laudantium facilis modi rem reiciendis, non recusandae deserunt magni dicta.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
